2666ed70fa0d6182a9d38a2c3abe10b4cb5742a7
jennyyosr <37708118+jennyyosr@users.noreply.github.com>
2018-05-05 20:49:14 +0200

    Add files via upload

--- base/hopital.sql
+++ base/hopital.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le :  ven. 04 mai 2018 à 18:56
--- Version du serveur :  10.1.30-MariaDB
--- Version de PHP :  7.2.1
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  sam. 05 mai 2018 à 18:48
+-- Version du serveur :  5.7.21
+-- Version de PHP :  5.6.35
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,9 +28,11 @@ SET time_zone = "+00:00";
 -- Structure de la table `admin`
 --
 
-CREATE TABLE `admin` (
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
   `user` varchar(50) NOT NULL,
-  `pass` varchar(50) NOT NULL
+  `pass` varchar(50) NOT NULL,
+  PRIMARY KEY (`user`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -46,11 +48,13 @@ INSERT INTO `admin` (`user`, `pass`) VALUES
 -- Structure de la table `chambre`
 --
 
-CREATE TABLE `chambre` (
+DROP TABLE IF EXISTS `chambre`;
+CREATE TABLE IF NOT EXISTS `chambre` (
   `numero` int(11) NOT NULL,
   `occupee` int(11) NOT NULL,
   `departement` int(50) NOT NULL,
-  `etage` varchar(10) NOT NULL
+  `etage` varchar(10) NOT NULL,
+  PRIMARY KEY (`numero`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -70,20 +74,27 @@ INSERT INTO `chambre` (`numero`, `occupee`, `departement`, `etage`) VALUES
 -- Structure de la table `departement`
 --
 
-CREATE TABLE `departement` (
+DROP TABLE IF EXISTS `departement`;
+CREATE TABLE IF NOT EXISTS `departement` (
   `id_departement` int(11) NOT NULL,
-  `nom_departement` varchar(100) NOT NULL,
-  `description` varchar(100) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `nom_departement` varchar(20) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  PRIMARY KEY (`id_departement`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `departement`
 --
 
 INSERT INTO `departement` (`id_departement`, `nom_departement`, `description`) VALUES
-(1, 'Cardiologie', ''),
-(4, 'Pediatrie', ''),
-(3, 'Neurologie', '');
+(408, 'radiologie', 'la radiologie designe l\'ensemble des modalites diagnostiques et therapeutiques utilisant les rayons X, ou plus generalement utilisant des rayonnements.'),
+(121, 'cardiologie', 'La cardiologie est la specialite medicale qui etudie le coeur et ses maladies'),
+(156, 'chirurgie', 'La chirurgie est la partie de la therapeutique qui implique des '),
+(113, 'dermatologie', 'La dermatologie est la branche de la medecine qui s\'occupe de la peau, des muqueuses et des phaneres (ongles, cheveux, poils)'),
+(112, 'immunologie', ' s\'occupe de l\'etude du systeme immunitaire.'),
+(315, 'maternites', 'Les maternites sont des lieux de sante assurant le suivi de la grossesse, l\'accouchement et les suites de couche de la femme enceinte, ou parturiente.'),
+(118, 'Neurologie', 'La neurologie est la discipline medicale clinique qui etudie l\'ensemble des maladies du systeme nerveux et en particulier du cerveau'),
+(308, 'MÃ©decine gÃ©nÃ©rale', 'est une specialite medicale prenant en charge le suivi durable, le bien-etre et les soins mÃ©dicaux gÃ©nÃ©raux primaires sans se limiter a des groupes de maladies relevant d\'un organe, d\'un age, ou d\'un sexe particulier.');
 
 -- --------------------------------------------------------
 
@@ -91,7 +102,8 @@ INSERT INTO `departement` (`id_departement`, `nom_departement`, `description`) V
 -- Structure de la table `lit`
 --
 
-CREATE TABLE `lit` (
+DROP TABLE IF EXISTS `lit`;
+CREATE TABLE IF NOT EXISTS `lit` (
   `id` int(11) NOT NULL,
   `chambre` varchar(10) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
@@ -102,20 +114,15 @@ CREATE TABLE `lit` (
 -- Structure de la table `medecin`
 --
 
-CREATE TABLE `medecin` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `medecin`;
+CREATE TABLE IF NOT EXISTS `medecin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(10) NOT NULL,
   `prenom` varchar(10) NOT NULL,
   `telephone` int(11) NOT NULL,
-  `departement` varchar(10) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `medecin`
---
-
-INSERT INTO `medecin` (`id`, `nom`, `prenom`, `telephone`, `departement`) VALUES
-(2, 'hayfa', 'tayeb', 23337901, 'Cardiologi');
+  `departement` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -123,29 +130,30 @@ INSERT INTO `medecin` (`id`, `nom`, `prenom`, `telephone`, `departement`) VALUES
 -- Structure de la table `patient`
 --
 
-CREATE TABLE `patient` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `patient`;
+CREATE TABLE IF NOT EXISTS `patient` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(20) NOT NULL,
   `prenom` varchar(20) NOT NULL,
   `sexe` varchar(10) NOT NULL,
   `date_naissance` date NOT NULL,
-  `numero` int(11) NOT NULL,
   `date_hospitalisation` date NOT NULL,
   `date_sortie` date NOT NULL,
   `chambre` varchar(10) NOT NULL,
   `departement` varchar(10) NOT NULL,
-  `antecedents_medicaux` varchar(250) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `antecedents_medicaux` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `patient`
 --
 
-INSERT INTO `patient` (`id`, `nom`, `prenom`, `sexe`, `date_naissance`, `numero`, `date_hospitalisation`, `date_sortie`, `chambre`, `departement`, `antecedents_medicaux`) VALUES
-(10, 'daoues', 'hamza', 'Homme', '2018-04-05', 0, '2018-04-04', '2018-04-12', '125', '10', 'jjj'),
-(9, 'Jenny', 'Yosr', 'Femme', '2018-04-11', 0, '2018-04-14', '2018-05-07', '100', '10', 'hhhred'),
-(8, 'salem', 'sinda', 'Femme', '2018-04-05', 0, '2018-04-07', '2018-04-15', '101', '10', 'jjhnh'),
-(7, 'Tayeb', 'Hayfa', 'Femme', '2018-04-10', 0, '2018-04-20', '0000-00-00', '200', '20', 'egngg nnn jfhc s');
+INSERT INTO `patient` (`id`, `nom`, `prenom`, `sexe`, `date_naissance`, `date_hospitalisation`, `date_sortie`, `chambre`, `departement`, `antecedents_medicaux`) VALUES
+(10, 'daoues', 'hamza', 'Homme', '2018-04-05', '2018-04-04', '2018-04-12', '125', '10', 'jjj'),
+(9, 'jenny', 'yosr', 'Femme', '2018-04-11', '2018-04-14', '0000-00-00', '100', '10', 'hhhred'),
+(8, 'salem', 'sinda', 'Femme', '2018-04-05', '2018-04-07', '2018-04-15', '101', '10', 'jjhnh'),
+(7, 'tayeb', 'hayfa', 'Femme', '2018-04-05', '2018-04-20', '0000-00-00', '200', '20', 'egngg nnn');
 
 -- --------------------------------------------------------
 
@@ -153,24 +161,25 @@ INSERT INTO `patient` (`id`, `nom`, `prenom`, `sexe`, `date_naissance`, `numero`
 -- Structure de la table `receptionniste`
 --
 
-CREATE TABLE `receptionniste` (
-  `id_receptionniste` int(55) NOT NULL,
+DROP TABLE IF EXISTS `receptionniste`;
+CREATE TABLE IF NOT EXISTS `receptionniste` (
+  `id_receptionniste` int(55) NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) NOT NULL,
   `prenom` varchar(50) NOT NULL,
   `sexe` varchar(50) NOT NULL,
   `date_naissance` date NOT NULL,
   `date_debuttravail` date NOT NULL,
   `user` varchar(50) NOT NULL,
-  `pass` varchar(50) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `pass` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_receptionniste`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `receptionniste`
 --
 
 INSERT INTO `receptionniste` (`id_receptionniste`, `nom`, `prenom`, `sexe`, `date_naissance`, `date_debuttravail`, `user`, `pass`) VALUES
-(1, 'walim', 'krichen', 'homme', '1998-01-27', '2017-01-01', 'walim', 'it\'sme'),
-(3, 'Hayfa', 'Tayeb', 'femme', '1997-10-23', '2018-05-09', 'hayfa', '0000');
+(1, 'walim', 'krichen', 'homme', '1998-01-27', '2017-01-01', 'walim', 'it\'sme');
 
 -- --------------------------------------------------------
 
@@ -178,100 +187,15 @@ INSERT INTO `receptionniste` (`id_receptionniste`, `nom`, `prenom`, `sexe`, `dat
 -- Structure de la table `visite`
 --
 
-CREATE TABLE `visite` (
-  `numero` int(11) NOT NULL,
-  `patient` int(11) NOT NULL,
-  `type` varchar(50) NOT NULL,
+DROP TABLE IF EXISTS `visite`;
+CREATE TABLE IF NOT EXISTS `visite` (
+  `id_patient` int(11) NOT NULL,
   `date` date NOT NULL,
   `taille` int(11) NOT NULL,
-  `poids` int(11) NOT NULL,
-  `diagnostique` varchar(250) NOT NULL,
-  `docteur` varchar(50) NOT NULL,
+  `poid` int(11) NOT NULL,
+  `diagnostique` int(11) NOT NULL,
   `departement` varchar(10) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `visite`
---
-
-INSERT INTO `visite` (`numero`, `patient`, `type`, `date`, `taille`, `poids`, `diagnostique`, `docteur`, `departement`) VALUES
-(1, 7, 'hosp', '2018-05-15', 152, 25, 'mlkjh', 'dr tgvcg', '20'),
-(2, 7, 'hosp', '2018-05-09', 152, 65, 'iuh', 'ghfcdc', '20'),
-(3, 7, 'hospitalÃ©', '2018-05-18', 452, 415, 'ooikhg', 'hbgg', '20');
-
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`user`);
-
---
--- Index pour la table `chambre`
---
-ALTER TABLE `chambre`
-  ADD PRIMARY KEY (`numero`);
-
---
--- Index pour la table `departement`
---
-ALTER TABLE `departement`
-  ADD PRIMARY KEY (`id_departement`);
-
---
--- Index pour la table `medecin`
---
-ALTER TABLE `medecin`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `patient`
---
-ALTER TABLE `patient`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `receptionniste`
---
-ALTER TABLE `receptionniste`
-  ADD PRIMARY KEY (`id_receptionniste`);
-
---
--- Index pour la table `visite`
---
-ALTER TABLE `visite`
-  ADD PRIMARY KEY (`numero`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `departement`
---
-ALTER TABLE `departement`
-  MODIFY `id_departement` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT pour la table `medecin`
---
-ALTER TABLE `medecin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT pour la table `receptionniste`
---
-ALTER TABLE `receptionniste`
-  MODIFY `id_receptionniste` int(55) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT pour la table `visite`
---
-ALTER TABLE `visite`
-  MODIFY `numero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
